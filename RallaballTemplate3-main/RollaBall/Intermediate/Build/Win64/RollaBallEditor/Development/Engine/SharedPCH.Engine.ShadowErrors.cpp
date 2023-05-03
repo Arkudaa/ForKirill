39a4394604e157bc312812fa083b1a354678e0fd
kirillChelovek2 <mr.kirill.petrochenko@gmail.com>
2023-05-03 18:45:28 +0300

    version01

---
+++ RallaballTemplate3-main/RollaBall/Intermediate/Build/Win64/RollaBallEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "C:/Users/info/OneDrive/Documents/Unreal Projects/RallaballTemplate3-main/RollaBall/Intermediate/Build/Win64/RollaBallEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
